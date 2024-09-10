f05019ac0abdb3572bf3b7d949da9da86c37aa75
mit-27 <meetsuthar64@gmail.com>
2024-09-09 19:58:52 -0600

    ✨ Add trpc in nextjs

--- apps/api/src/trpc/trpc.service.ts
+++ apps/api/src/trpc/trpc.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { initTRPC } from '@trpc/server';
+import { transformer } from './transformer';
+// import superjson from "superjson";
 
 @Injectable()
 export class TrpcService {
-    trpc = initTRPC.create();
+    trpc = initTRPC.create({ transformer });
     procedure = this.trpc.procedure;
     router = this.trpc.router;
     mergeRouters = this.trpc.mergeRouters;
